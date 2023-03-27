a44e745f8035b41e39155cf2539a5d3448f3b011
ehnvik <gustav.ehnvik@gmail.com>
2023-03-27 11:18:08 +0200

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express from "express";
+const cors = require("cors");
+
+const app = express();
+
+app.use(
+  cors({
+    methods: ["GET", "PUT", "PATCH", "DELETE", "POST"],
+  })
+);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// app.use("/");
+
+const port = process.env.PORT || 8008;
+const run = () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is listening on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+run();
